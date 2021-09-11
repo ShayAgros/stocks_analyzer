72ef8f32a78f6ee2d10a790a65ced4a791f6d554
Shay Agroskin <agrosshay@gmail.com>
2021-09-11 21:38:45 +0300

    ticker_gui: Separate ticker table graphic to separate file
    The ticker table view was moved to its own file in a new 'gui'
    directory (where more GUI related objects would be added eventually).
    
    The tickers table was also added some basic key event handling.
    
    Signed-off-by: Shay Agroskin <agrosshay@gmail.com>

---
+++ gui/ticker_table.py
@@ -0,0 +1,84 @@
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtCore import Qt, QEvent, QItemSelection, QItemSelectionModel
+from PyQt5.QtWidgets import (QTableView)
+
+from ticker import Ticker
+
+MAX_DISPLAY_ENTRIES = 14
+
+class TickersTableModel(QtCore.QAbstractTableModel):
+
+    def __init__(self, data):
+        super(TickersTableModel, self).__init__()
+
+        self._data = data
+        # This would allow us to access the "healthy" column number easier
+        # (needed when presenting the data)
+        self._healthy_col_ix = data.columns.get_loc("healthy")
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            # See below for the nested-list data structure.
+            # .row() indexes into the outer list,
+            # .column() indexes into the sub-list
+            value = self._data.iloc[index.row(), index.column()]
+
+            if isinstance(value, float):
+                return "%.3f" % value
+
+            return str(value)
+
+    def rowCount(self, index):
+        return self._data.shape[0]
+
+    def columnCount(self, index):
+        return min(self._data.shape[1], MAX_DISPLAY_ENTRIES)
+
+    def headerData(self, section, orientation, role):
+        # section is the index of the column/row.
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                header = str(self._data.columns[section])
+                header = header.replace("_", " ").title()
+                return header
+
+            if orientation == Qt.Vertical:
+                return str(self._data.index[section])
+
+        if role == Qt.ForegroundRole:
+            if orientation == Qt.Vertical:
+                healthy = self._data.iloc[section, self._healthy_col_ix]
+                color = "green" if healthy else "red"
+                return QtGui.QColor(color)
+
+class TickersTableView(QTableView):
+
+    def __init__(self, parent = None):
+        super().__init__(parent)
+
+        self.parent = parent
+
+    def keyPressEvent(self, e):
+
+        key = e.key()
+
+        if key == Qt.Key_Escape or e.text() == 'q':
+            self.parent.keyPressEvent(e)
+        elif key == Qt.Key.Key_Return:
+            print("enter has been pressed")
+            selectionModel = self.selectionModel()
+            current = selectionModel.currentIndex()
+
+            model = self.model()
+
+            data = model.data(current, Qt.DisplayRole)
+            header = model.headerData(current.row(), Qt.Vertical, Qt.DisplayRole)
+            print(f"cell data is {data}, header is {header}")
+
+            # symbol and market array
+            symbol, market = header.split(":")
+            print(symbol, market)
+            ticker = Ticker(symbol, market)
+            ticker.plot_me()
+        else:
+            super().keyPressEvent(e)
--- ticker_gui.py
+++ ticker_gui.py
@@ -1,63 +1,15 @@
 #!/usr/bin/env python3
 
 from PyQt5 import QtWidgets, QtGui, QtCore
-from PyQt5.QtCore import Qt, QEvent
+from PyQt5.QtCore import Qt, QEvent, QItemSelection, QItemSelectionModel
 from PyQt5.QtWidgets import (QApplication, QVBoxLayout, QWidget, QTableWidget, QTableWidgetItem)
+
+from gui.ticker_table import TickersTableView, TickersTableModel
+
 import pandas as pd
 import sys
 
 import stocks_analyzer
-
-MAX_DISPLAY_ENTRIES = 14
-
-class TickersTableModel(QtCore.QAbstractTableModel):
-
-    def __init__(self, data):
-        super(TickersTableModel, self).__init__()
-
-        self._data = data
-        # This would allow us to access the "healthy" column number easier
-        # (needed when presenting the data)
-        self._healthy_col_ix = data.columns.get_loc("healthy")
-
-    def data(self, index, role):
-        if role == Qt.DisplayRole:
-            # See below for the nested-list data structure.
-            # .row() indexes into the outer list,
-            # .column() indexes into the sub-list
-            value = self._data.iloc[index.row(), index.column()]
-
-            if isinstance(value, float):
-                return "%.3f" % value
-
-            return str(value)
-
-    def rowCount(self, index):
-        return self._data.shape[0]
-
-    def columnCount(self, index):
-        return min(self._data.shape[1], MAX_DISPLAY_ENTRIES)
-
-    def headerData(self, section, orientation, role):
-        # section is the index of the column/row.
-        if role == Qt.DisplayRole:
-            if orientation == Qt.Horizontal:
-                header = str(self._data.columns[section])
-                header = header.replace("_", " ").title()
-                return header
-
-            if orientation == Qt.Vertical:
-                return str(self._data.index[section])
-
-        if role == Qt.ForegroundRole:
-            if orientation == Qt.Vertical:
-                healthy = self._data.iloc[section, self._healthy_col_ix]
-                color = "green" if healthy else "red"
-                # healthy_col_ix = 
-                # print(healthy_col_ix)
-                # print(self._data.iloc[section, healthy_col_ix])
-                # print(healthy)
-                return QtGui.QColor(color)
 
 class tickers_gui(QWidget):
 
@@ -77,13 +29,18 @@ class tickers_gui(QWidget):
 
         vlayout = QVBoxLayout()
 
-        self.tickers_table = QtWidgets.QTableView()
+        self.tickers_table = TickersTableView(self)
         self.model = TickersTableModel(tickers_stats)
 
         header = self.tickers_table.horizontalHeader()
         header.setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
 
         self.tickers_table.setModel(self.model)
+
+        # selection_model = self.tickers_table.selectionModel()
+        # top_left, bottom_right = self.model.index(0, 0), self.model.index(10, 10)
+        # selection = QItemSelection(top_left, bottom_right)
+        # selection_model.select(selection, QItemSelectionModel.SelectionFlag.Select)
         
         vlayout.addWidget(self.tickers_table)
 
@@ -102,7 +59,7 @@ if __name__ == '__main__':
     tickers = stocks_analyzer.create_tickers_from_file('./russel_formated_first_50.txt')
 
 
-    d = {(ticker.symbol): ticker.statistics.values() for ticker in tickers}
+    d = {(f"{ticker.symbol}:{ticker.market}" ): ticker.statistics.values() for ticker in tickers}
     # ---- As Dataframe: ----
     df = pd.DataFrame.from_dict(d, orient='index', columns=tickers[0].statistics.keys())
 
